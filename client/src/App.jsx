a0a1d13629232b724e743a05ed82b28cc47ff69a
Bhagibhagya <koruprolubhagyaraju@gmail.com>
2024-10-17 12:27:11 +0530

    creating a pages and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
